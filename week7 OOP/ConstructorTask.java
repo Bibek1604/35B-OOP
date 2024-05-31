527788f857d392aded7747b2e6b7bec57a3a8fe6
Bibek1604 <npbibek94@gmail.com>
2024-05-31 15:48:02 +0545

    task and work added

--- week7 OOP/ConstructorTask.java
+++ week7 OOP/ConstructorTask.java
@@ -1,28 +1,11 @@
-
-/* make a class cat
- * make 2 private attribute name,age
- * make 2 public attribute breed,color
- * make a constructor to nly st na,e amd age
- * ake fetter for name and age
- * make setter for age
- * ,ake an object of cat
- * fill the attribute
- * add +1 to the current page
- * print the following using obkejct
- * name:ny
- * age:4
- * Breed:xy
- * Color:orange
- */
 public class ConstructorTask {
     public static void main(String[] args) {
-        Cat myCat = new Cat("Ny", 3);
+        Cat myCat = new Cat("Ny", 3); 
 
         myCat.breed = "xy"; 
         myCat.color = "orange"; 
 
         myCat.setAge(myCat.getAge() + 1); 
-
         System.out.println("Name: " + myCat.getName());
         System.out.println("Age: " + myCat.getAge());
         System.out.println("Breed: " + myCat.breed);
