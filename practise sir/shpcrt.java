260045ea8888f2e6836b06f456596717cdac74fc
Bibek1604 <npbibek94@gmail.com>
2024-07-10 21:47:04 +0545

    arrays not used here

---
+++ practise sir/shpcrt.java
@@ -0,0 +1,44 @@
+// /*create a shopping cart class to simlulate an online shopping cart.the class should have the follwoing attributes: cartId,customer name,and 
+// items(list of products) include method to additem() rmeoveitem() calculatetotal() implement funcitonlaity to creaate new shopping cart
+// add/remove tems and calculate total cost of item in the cart  */
+// public class shpcrt {
+//     public static void main(String[] args) {
+        
+//     }
+    
+// }
+
+// class Shoppingcart{
+//     public int cartId;
+//     public String customer_name;
+//     public String Items;
+
+//     Shoppingcart(int cartId , String customer_name,String Items) {
+//         this.cartId=cartId;
+//         this.customer_name=customer_name;
+//         this.Items=Items;
+
+//     }
+
+//     public void additem(){
+//         Items = Items+Items;
+        
+//     }
+//     public void rmeoveitem(){
+//         if (Items<0){
+//             Items = Items -Items;
+//         }else{
+//         System.out.println("There is no items");
+
+//         }
+//     }
+//     public void price(int amount){
+//         amount = Items *amount;
+        
+//     }
+//     public int totalprice(){
+//         return price;
+
+//     }
+
+// }
