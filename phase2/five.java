23607d4c95620a96829406320fcbaf3a0897eb08
Bibek1604 <npbibek94@gmail.com>
2024-08-10 09:22:39 +0545

    km converted

---
+++ phase2/five.java
@@ -0,0 +1,10 @@
+public class five {
+    public static void main(String[] args) {
+        System.out.println("miles\tKilometer");
+        for (int miles = 1; miles <= 10; miles++) {
+            double a = miles * 1.609;
+            System.out.println(miles + "\t" + a);
+        }
+
+    }
+}
